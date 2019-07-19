61b0e3cc001f1158cb59eea94c945e77c9397812
Your Name <you@example.com>
2019-07-19 11:44:39 +0800

    dao

---
+++ UserDAO.java
@@ -0,0 +1,16 @@
+import java.util.List;
+
+import edu.fzu.tmall.pojo.User;
+
+public interface UserDAO {
+    int getTotal();
+    void add(User user);
+    void update(User user);
+    void delete(int id) ;
+    User get(int id);
+    List<User> list();
+    List<User> list(int start, int count) ;
+    boolean isExist(String name) ;
+    User get(String name) ;
+    User get(String name, String password) ;
+}
